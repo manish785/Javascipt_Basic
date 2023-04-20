27cf7dcc1a005bf51b08132b0885116547324491
manish785 <kumarachilish1997@gmail.com>
2023-04-20 10:54:18 +0530

    going through JS basics

---
+++ JavascriptBasic.js
@@ -0,0 +1,42 @@
+// javascript is lossely typed as it holds interger, character, boolean etc
+var a = 10;
+console.log(a);
+
+a = "manish";
+console.log(a);
+
+a = 10.5;
+console.log(a);
+
+var b = 10.6;
+console.log(b);
+
+// variable declarations and definitions
+var a ;
+console.log(a);
+
+var b = 10;
+console.log(b);
+
+// null and undefined
+var c = null; // absence of value, want to set value but currently have no value
+console.log(c);
+
+// NAN
+console.log(Math.sqrt(-1))
+console.log(parseInt('aaaaaaa'));
+
+//type of 
+var x = null;
+console.log(typeof(x));
+
+console.log(typeof(NAN))
+
+
+
+
+
+
+
+
+
